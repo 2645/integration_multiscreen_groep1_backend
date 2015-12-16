cb7de67fb26dc127cf3bd04d2062bc6f7b436d14
Dieter Holvoet <dieter.holvoet@gmail.com>
2015-12-16 12:29:46 +0100

    Avatar controller bugfixes

--- be.ehb.2ba.groep1_RestServerMetDatabase_jar_1.0-SNAPSHOT/src/main/java/be/ehb/restservermetdatabase/dao/AvatarDao.java
+++ be.ehb.2ba.groep1_RestServerMetDatabase_jar_1.0-SNAPSHOT/src/main/java/be/ehb/restservermetdatabase/dao/AvatarDao.java
@@ -31,7 +31,7 @@ public class AvatarDao {
             ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from usersavatars WHERE user_id = ?", new Object[]{user_id});
             if (mijnResultset != null) {
                 while (mijnResultset.next()) {
-                    Avatar huidigeAvatar = converteerHuidigeRijNaarObject(mijnResultset);
+                    Avatar huidigeAvatar = AvatarDao.getAvatarById(mijnResultset.getInt("avatar_id"));
                     resultaat.add(huidigeAvatar);
                 }
             }
@@ -59,10 +59,26 @@ public class AvatarDao {
         return resultaat;
     }
 
+    public static Avatar getAvatarByName(String name) {
+        Avatar resultaat = null;
+        try {
+            ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from avatars where avatar_name = ?", new Object[]{name});
+            if (mijnResultset != null) {
+                mijnResultset.first();
+                resultaat = converteerHuidigeRijNaarObject(mijnResultset);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            // Foutafhandeling naar keuze
+        }
+
+        return resultaat;
+    }
+
     public static int voegAvatarToe(Avatar nieuweAvatar) {
         int aantalAangepasteRijen = 0;
         try {
-            aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("INSERT INTO avatars ( avatar_name, avatar_img ) VALUES (?,?)", new Object[]{nieuweAvatar.getAvatar_name(), nieuweAvatar.getAvatar_img()});
+            aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("INSERT INTO avatars ( avatar_name, avatar_img, avatar_price ) VALUES (?,?,?)", new Object[]{nieuweAvatar.getAvatar_name(), nieuweAvatar.getAvatar_img(), nieuweAvatar.getAvatar_price()});
         } catch (SQLException ex) {
             ex.printStackTrace();
             // Foutafhandeling naar keuze
@@ -72,12 +88,33 @@ public class AvatarDao {
 
     public static int updateAvatar(Avatar nieuweAvatar) {
         int aantalAangepasteRijen = 0;
+        int avatar_id = nieuweAvatar.getAvatar_id();
+        
+        if(avatar_id == 0) {
+            return 0;
+            
+        } else {
+            if(nieuweAvatar.getAvatar_img().equals("")) {
+                nieuweAvatar.setAvatar_img(AvatarDao.getAvatarById(avatar_id).getAvatar_img());
+            }
+            
+            if(nieuweAvatar.getAvatar_name().equals("")) {
+                nieuweAvatar.setAvatar_name(AvatarDao.getAvatarById(avatar_id).getAvatar_name());
+            }
+            
+            if(nieuweAvatar.getAvatar_price() == 0) {
+                nieuweAvatar.setAvatar_price(AvatarDao.getAvatarById(avatar_id).getAvatar_price());
+            }
+        }
+        
         try {
-            aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("UPDATE avatars SET avatar_name = ?, avatar_img = ? WHERE avatar_id = ?", new Object[]{nieuweAvatar.getAvatar_name(), nieuweAvatar.getAvatar_img(), nieuweAvatar.getAvatar_id()});
+            aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("UPDATE avatars SET avatar_name = ?, avatar_img = ?, avatar_price = ? WHERE avatar_id = ?", new Object[]{nieuweAvatar.getAvatar_name(), nieuweAvatar.getAvatar_img(), nieuweAvatar.getAvatar_price(), avatar_id});
+        
         } catch (SQLException ex) {
             ex.printStackTrace();
             // Foutafhandeling naar keuze
         }
+        
         return aantalAangepasteRijen;
     }
 
--- be.ehb.2ba.groep1_RestServerMetDatabase_jar_1.0-SNAPSHOT/src/main/java/be/ehb/restservermetdatabase/webservice/AvatarController.java
+++ be.ehb.2ba.groep1_RestServerMetDatabase_jar_1.0-SNAPSHOT/src/main/java/be/ehb/restservermetdatabase/webservice/AvatarController.java
@@ -34,39 +34,38 @@ public class AvatarController {
     }
     
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public ArrayList<Avatar> list(@RequestParam(value = "user_id", defaultValue = "") int user_id, @RequestParam(value = "user_id", defaultValue = "") String user_mail) {
+    public ArrayList<Avatar> list(@RequestParam(value = "user_id", defaultValue = "0") int user_id, @RequestParam(value = "user_mail", defaultValue = "") String user_mail) {
         if (user_id == 0 && user_mail.equals("")) {
             return AvatarDao.getAvatars();
             
-        } else if(!user_mail.equals("")) {
-            user_id = UserDao.getUserByEmail(user_mail).getUser_id();
-            return AvatarDao.getAvatarsByUser(user_id);
-            
         } else {
-            return null;
+            if(!user_mail.equals("")) {
+                user_id = UserDao.getUserByEmail(user_mail).getUser_id();
+            }
+            return AvatarDao.getAvatarsByUser(user_id);
         }
     }
     
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public int create(
             @RequestParam(value = "name", defaultValue = "") String name, 
-            @RequestParam(value = "price", defaultValue = "") int price, 
+            @RequestParam(value = "price", defaultValue = "0") int price, 
             @RequestParam(value = "img", defaultValue = "") String img
     ) {
         
-        int avatarID = AvatarDao.voegAvatarToe(new Avatar(name, img, price));
-        return AvatarDao.getAvatarById(avatarID).getAvatar_id();
+        AvatarDao.voegAvatarToe(new Avatar(name, img, price));
+        return AvatarDao.getAvatarByName(name).getAvatar_id();
     }
     
     @RequestMapping(value = "/update", method = RequestMethod.GET)
     public int update(
-            @RequestParam(value = "name", defaultValue = "") int id, 
+            @RequestParam(value = "id", defaultValue = "0") int id, 
             @RequestParam(value = "name", defaultValue = "") String name, 
-            @RequestParam(value = "price", defaultValue = "") int price, 
+            @RequestParam(value = "price", defaultValue = "0") int price, 
             @RequestParam(value = "img", defaultValue = "") String img
     ) {
         
-        int avatarID = AvatarDao.updateAvatar(new Avatar(name, img, price));
-        return AvatarDao.getAvatarById(avatarID).getAvatar_id();
+        AvatarDao.updateAvatar(new Avatar(id, name, img, price));
+        return AvatarDao.getAvatarByName(name).getAvatar_id();
     }
 }
